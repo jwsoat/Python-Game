f94ee56867dec4df69f9663a678a3c669749b73b
Jwsoat <jwsoat@users.noreply.github.com>
2019-08-12 22:08:55 +1200

    Version 0.0.1

---
+++ All in one v0.1.py
@@ -0,0 +1,19 @@
+import math
+import time
+
+
+gameselector = input("Pick a Game")
+
+if gameselector == "1":
+    print ("Game 1 Selected")
+
+
+elif gameselector == "2":
+    print ("Game 2 Selected")
+
+
+elif gameselector == "3":
+    print ("Game 3 Selected")
+
+else:
+   print ("error")
